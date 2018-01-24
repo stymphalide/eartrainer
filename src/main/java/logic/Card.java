e568b31659773958b29e010fb94959688d074578
Tobias <tobias@bimbam.ch>
2018-01-24 16:02:04 +0100

    Implemented card class.

--- src/main/java/logic/Card.java
+++ src/main/java/logic/Card.java
@@ -2,5 +2,15 @@ package logic;
 
 
 public class Card {
+    Instrument instrument;
+    Interval interval;
+    Range range;
+    Order order;
     
+    public Card(Instrument instrument, Interval interval, Range range, Order order) {
+    	this.instrument = instrument;
+    	this.interval = interval;
+    	this.range = range;
+    	this.order = order;
+    }
 }
