acb99e5f273ed960ba5a397949adcbfbbdba91c9
Tobias <tobias@bimbam.ch>
2018-01-24 15:16:06 +0100

    Implemented allowed Instruments into level 1.

--- src/main/java/logic/Level1.java
+++ src/main/java/logic/Level1.java
@@ -1,16 +1,20 @@
 package logic;
 
+import java.util.*;
+
 public class Level1 extends Level {
 
 	private int correctAnswers;
 	private int wrongAnswers;
 	private int totalQuestions;
+	private List<Instrument> allowedInstruments;
 	
 	public Level1() {
 		super(1);
 		this.correctAnswers = 0;
 		this.wrongAnswers = 0;
 		this.totalQuestions = 10;
+		this.allowedInstruments = instantiateInstruments("Piano", "Strings", "Brass");
 	}
 
 	@Override
@@ -34,8 +38,20 @@ public class Level1 extends Level {
 	}
 
 	@Override
+	public List<Instrument> getAllowedInstruments(){
+		return this.allowedInstruments;
+	}
+	
+	@Override
 	public void play() {
 		// TODO Auto-generated method stub
 	}
 
+	private ArrayList<Instrument> instantiateInstruments(String instrument1, String instrument2, String instrument3){
+		ArrayList<Instrument> instruments = new ArrayList();
+		instruments.add(new Instrument(instrument1));
+		instruments.add(new Instrument(instrument2));
+		instruments.add(new Instrument(instrument3));
+		return instruments;
+	}
 }
