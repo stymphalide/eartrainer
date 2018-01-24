44c33f3cb7194021ce5668ce97f6f201c2918ee9
stymphalide <angelo.birrer@gmail.com>
2018-01-24 15:16:13 +0100

    Created constructor for class Instrument.

--- src/main/java/logic/Instrument.java
+++ src/main/java/logic/Instrument.java
@@ -2,5 +2,12 @@ package logic;
 
 
 public class Instrument {
-    
+    String name;
+
+    public Instrument(String instrument) {
+        this.name = instrument;
+    }
+    public String getStaccatoString(Range range) {
+        return name;
+    }
 }
