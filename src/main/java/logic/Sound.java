5faa97f0bc58e1276aca041b7b1f96e8c983f0af
Tobias <tobias@bimbam.ch>
2018-02-13 16:40:33 +0100

    The order now affects the interval played.

--- src/main/java/logic/Sound.java
+++ src/main/java/logic/Sound.java
@@ -4,7 +4,17 @@ import org.jfugue.player.Player;
 public class Sound {
 	
 	public String cardToStaccato(Card card) {
-		return "V0 I["+card.instrument+"] "+card.getNote1()+"h Rh V1 I["+card.instrument+"] Rh "+card.getNote2()+"h";
+		String staccato;
+		if (card.getOrder() == "upwards") {
+			staccato = "V0 I["+card.getInstrument()+"] "+card.getNote1()+"h Rh V1 I["+card.getInstrument()+"] Rh "+card.getNote2()+"h";
+		}
+		else if (card.getOrder() == "downwards") {
+			staccato = "V0 I["+card.getInstrument()+"] "+card.getNote2()+"h Rh V1 I["+card.getInstrument()+"] Rh "+card.getNote1()+"h";
+		}
+		else {
+			staccato = "V0 I["+card.getInstrument()+"] "+card.getNote1()+"h V1 I["+card.getInstrument()+"] "+card.getNote2()+"h";
+		}
+		return staccato;
 	}
 	
 	public void play(Card card) {
