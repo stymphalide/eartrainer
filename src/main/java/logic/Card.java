b49c8f4c2a70b845857039de1c0b7b0daa17b2e1
Tobias <tobias@bimbam.ch>
2018-02-17 23:09:23 +0100

    Implemented interval sizes.

--- src/main/java/logic/Card.java
+++ src/main/java/logic/Card.java
@@ -8,6 +8,7 @@ public class Card {
 	private String order;
 	private String range;
 	private String interval;
+	private HashMap intervalSizes;
     
     public Card(String instrument, 
     			String order,
@@ -36,13 +37,25 @@ public class Card {
     	return this.interval;
     }
     
-    
-    /*private void setIntervalSizes() {
+    private void setIntervalSizes() {
     	this.intervalSizes = new HashMap<String, Integer>();
-        this.intervalSizes.put("Perfect Fourth", 5);
+    	this.intervalSizes.put("Minor Second", 1);
+    	this.intervalSizes.put("Major Second", 2);
+    	this.intervalSizes.put("Minor Third", 3);
+    	this.intervalSizes.put("Major Third", 4);
+    	this.intervalSizes.put("Perfect Fourth", 5);
+    	this.intervalSizes.put("Tritone", 6);
         this.intervalSizes.put("Perfect Fifth", 7);
+        this.intervalSizes.put("Minor Sixth", 8);
+        this.intervalSizes.put("Major Sixth", 9);
+        this.intervalSizes.put("Minor Seventh", 10);
+        this.intervalSizes.put("Major Seventh", 11);
         this.intervalSizes.put("Perfect Octave", 12);
-    }*/
+        this.intervalSizes.put("Minor Ninth", 13);
+        this.intervalSizes.put("Major Ninth", 14);
+        this.intervalSizes.put("Minor Tenth", 15);
+        this.intervalSizes.put("Major Tenth", 16);
+    }
     
     //TODO: Implement this.
     public String getNote1() {    	
