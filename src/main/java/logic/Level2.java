1c8b178bfc40c31462187e7a62fa94ba25606542
ScarlocNebelwandler <tobias@bimbam.ch>
2018-03-02 13:24:44 +0100

    Solved the harpsichord problem.

--- src/main/java/logic/Level2.java
+++ src/main/java/logic/Level2.java
@@ -19,7 +19,7 @@ public class Level2 extends Level {
 		this.correctAnswers = 0;
 		this.wrongAnswers = 0;
 		this.totalQuestions = 10;
-		this.allowedInstruments = instantiateFeatures("Piano", "Harpischord", "Organ");
+		this.allowedInstruments = instantiateFeatures("Piano", "Harpsichord", "Organ");
 		this.allowedOrders = instantiateFeatures("Upwards", "Downwards", "Chordal");
 		this.allowedRanges = instantiateFeatures("Low", "Middle", "High");
 		this.allowedIntervals = instantiateFeatures("Minor Second", "Major Second", "Minor Third");
