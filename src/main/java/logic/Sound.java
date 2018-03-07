c9d3d30b0efbdf280d8f54bb1bcf2f5682afd2fb
stymphalide <angelo.birrer@gmail.com>
2018-03-07 14:52:51 +0100

    Implemented the Sound as a Thread Level class not touched.

--- src/main/java/logic/Sound.java
+++ src/main/java/logic/Sound.java
@@ -2,38 +2,40 @@ package logic;
 import org.jfugue.*;
 
 public class Sound extends Thread {
-	private Card card;
-	@Override
-	public void run() {
-		synchronized(this) {
-			while(true) {
-				while(card == null) wait();
+    private Card card;
+    public void run() {
+        while(true) {
+            try {
+                synchronized(this) {
+                    while(card == null) wait();
 
-				Player player = new Player();
-				String stac = this.cardToStaccato(card);
-				this.card = null;
-				//System.out.println(stac);
-				player.play(stac);
-			}
-		}
-	}
+                    Player player = new Player();
+                    String stac = this.cardToStaccato(card);
+                    this.card = null;
+                    //System.out.println(stac);
+                    player.play(stac);
+                }
+            
+            } catch (InterruptedException e){}
+        }
+    }
 
-	public String cardToStaccato(Card card) {
-		String staccato;
-		if (card.getOrder() == "Upwards") {
-			staccato = "V0 I["+card.getActualInstrument()+"] ["+card.getNote1()+"]h ["+card.getNote2()+"]h";
-		}
-		else if (card.getOrder() == "Downwards") {
-			staccato = "V0 I["+card.getActualInstrument()+"] ["+card.getNote2()+"]h ["+card.getNote1()+"]h";
-		}
-		else {
-			staccato = "V0 I["+card.getActualInstrument()+"] ["+card.getNote1()+"]h.+["+card.getNote2()+"]h.";
-		}
-		return staccato;
-	}
-	
-	public void play(Card card) {
-		this.card = card;
-		this.notify();
-	}
+    public String cardToStaccato(Card card) {
+        String staccato;
+        if (card.getOrder() == "Upwards") {
+            staccato = "V0 I["+card.getActualInstrument()+"] ["+card.getNote1()+"]h ["+card.getNote2()+"]h";
+        }
+        else if (card.getOrder() == "Downwards") {
+            staccato = "V0 I["+card.getActualInstrument()+"] ["+card.getNote2()+"]h ["+card.getNote1()+"]h";
+        }
+        else {
+            staccato = "V0 I["+card.getActualInstrument()+"] ["+card.getNote1()+"]h.+["+card.getNote2()+"]h.";
+        }
+        return staccato;
+    }
+    
+    public void play(Card card) {
+        this.card = card;
+        this.notify();
+    }
 }
