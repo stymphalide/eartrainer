0d0acac58f6c93bb59678ef1f34e0451b2698bfd
stymphalide <angelo.birrer@gmail.com>
2018-03-13 22:20:51 +0100

    Added a new private class that holds the Ranking Values.

--- src/main/java/view/Ranking.java
+++ src/main/java/view/Ranking.java
@@ -30,6 +30,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Ranking extends Group {
     Scene scene;
+    TableView table;
 
     public static void updateRanking(logic.Level level) {
         Path file = FileSystems.getDefault().getPath("./resources/ranking.csv");
@@ -53,7 +54,7 @@ public class Ranking extends Group {
     }
 
     public Ranking() {
-
+        setUpTable();
     }
     public Scene render() {
         if(this.scene == null) {
@@ -65,6 +66,115 @@ public class Ranking extends Group {
     }
     private void update() {
 
+    }
+
+    private void setUpTable() {
+        TableColumn nameCol = new TableColumn("Name");
+        TableColumn levelCol = new TableColumn("Level");
+        TableColumn dateCol = new TableColumn("Date");
+        TableColumn timeCol = new TableColumn("Time");
+        TableColumn correctCol = new TableColumn("Correct");
+
+        this.table = new TableView<>();
+        table.setItems(getValues());
+        table.getColums().addAll(
+            nameCol, 
+            levelCol, 
+            dateCol, 
+            timeCol, 
+            correctCol);
+        this.getChildren().add(this.table);
+
+    }
+
+    private ObservableList getValues() {
+        ObservableList values = FXCollections.observableArrayList();
+        Path file = FileSystems.getDefault().getPath("./resources/ranking.csv");
+        Charset charset = Charset.forName("UTF-8");
+        // Open file
+        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+            String line = null;
+            boolean header = true;
+            while ((line = reader.readLine()) != null) {
+                if(header) {
+                    header = !header;
+                } else {
+                    values.add(new RankingVal(line));
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    return values;
+}
+private class RankingVal {
+    private String name;
+    private int level;
+    private long date;
+    private long time;
+    private double correct;
+    public RankingVal(String name, 
+                      int level, 
+                      long date, 
+                      long time, 
+                      double correct) {
+        this.name = name;
+        this.level = level;
+        this.date = date;
+        this.time = time;
+        this.correct = correct;
+    }
+    public RankingVal(logic.Level level) throws Exception {
+        if(level.isFinished()) {
+            this.name = "Some Name";
+            this.level = level.getLevelNumber();
+            this.date = level.getStartTime().getEpochSeconds();
+            this.time = level.getDuration().getSeconds();
+            this.correct = (double)(level.getCorrectAnswers() / level.getWrongAnswers());
+        } else {
+            throw new java.lang.RuntimeException("The level must be finished.");
+        }
+    }
+    public RankingVal(String line) {
+        String[] values = line.split(",");
+        this.name = values[0];
+        this.level = Integer.parseInt(values[1]);
+        this.date = Long.parseLong(values[2]);
+        this.time = Long.parseLong(values[3]);
+        this.correct = Double.parseDouble(values[4]);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getLevel() {
+        return this.level;
+    }
+    public String getDate() {
+        return "No Date";
+    }
+    public String getTime() {
+        return "No Time";
+    }
+    public double getCorrect() {
+        return this.correct;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public void setDate(long date) {
+        this.date = date;
+    }
+    public void setTime(long time) {
+        this.time = time;
+    }
+    public void setCorrect(double correct) {
+        this.correct = correct;
     }
 
 
