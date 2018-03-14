703de20fee30d45f8cbe328c773a3d6dca07d669
stymphalide <angelo.birrer@gmail.com>
2018-03-14 23:03:36 +0100

    The Leaderboard now shows the actual values.

--- src/main/java/view/Ranking.java
+++ src/main/java/view/Ranking.java
@@ -19,6 +19,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.property.SimpleStringProperty;
+
 
 /* classdoc
     Opens a .csv file and renders it.
@@ -32,30 +36,37 @@ import javafx.scene.control.cell.PropertyValueFactory;
 */
 
 public class Ranking extends Group {
-    Scene scene;
-    TableView table;
-    Leaderboard leaderboard;
+    private Scene scene;
+    private TableView table;
+    private int levelNumber;
 
     public static void updateRanking(logic.Level level) {
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        RankingVal rank;
-        System.out.println(level);
-        rank = new RankingVal(level, "Angelo");
-        System.out.println(gson.toJson(rank));
-        Path file = FileSystems.getDefault().getPath("./resources/ranking.json");
-        // Open file
-        Leaderboard leaderboard;
-        try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8)) {
-             leaderboard = gson.fromJson(reader, Leaderboard.class);
-             leaderboard.add(rank, level.getLevelNumber());
-             System.out.println(gson.toJson(leaderboard));
+        RankingVal rank = new RankingVal(level, "Angelo");
+        try {
+            Leaderboard leaderboard = openRankingFile();
+            leaderboard.add(rank, level.getLevelNumber());
+            
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            
+
+            System.out.println(gson.toJson(leaderboard));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    private static Leaderboard openRankingFile() throws IOException {
+        Path file = FileSystems.getDefault().getPath("./resources/ranking.json");
+
+        BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.fromJson(reader, Leaderboard.class);
+    }
 
     public Ranking() {
+        this.levelNumber = 1;
         setUpTable();
     }
     public Scene render() {
@@ -71,16 +82,42 @@ public class Ranking extends Group {
     }
     private void setUpTable() {
         TableColumn nameCol = new TableColumn("Name");
-        TableColumn levelCol = new TableColumn("Level");
+        nameCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<RankingVal, String>, ObservableValue<String>>() {
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<RankingVal, String> value) {
+                SimpleStringProperty obsVal = new SimpleStringProperty();
+                obsVal.set(value.getValue().getName());
+                return obsVal;
+            }
+        });
         TableColumn dateCol = new TableColumn("Date");
+        dateCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<RankingVal, String>, ObservableValue<String>>() {
+           public ObservableValue<String> call(TableColumn.CellDataFeatures<RankingVal, String> value) {
+                SimpleStringProperty obsVal = new SimpleStringProperty();
+                obsVal.set(value.getValue().getDateString());
+                return obsVal;
+            } 
+        });
         TableColumn timeCol = new TableColumn("Time");
+        timeCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<RankingVal, String>, ObservableValue<String>>() {
+           public ObservableValue<String> call(TableColumn.CellDataFeatures<RankingVal, String> value) {
+                SimpleStringProperty obsVal = new SimpleStringProperty();
+                obsVal.set(value.getValue().getTimeString());
+                return obsVal;
+            } 
+        });
         TableColumn correctCol = new TableColumn("Correct");
+        correctCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<RankingVal, String>, ObservableValue<String>>() {
+           public ObservableValue<String> call(TableColumn.CellDataFeatures<RankingVal, String> value) {
+                SimpleStringProperty obsVal = new SimpleStringProperty();
+                obsVal.set(value.getValue().getCorrectString());
+                return obsVal;
+            } 
+        });
 
         this.table = new TableView<>();
         this.table.setItems(getValues());
         this.table.getColumns().addAll(
             nameCol, 
-            levelCol, 
             dateCol, 
             timeCol, 
             correctCol);
@@ -88,8 +125,16 @@ public class Ranking extends Group {
     }
 
     private ObservableList getValues() {
-        ObservableList values = FXCollections.observableArrayList();
-        
+        ObservableList<RankingVal> values = FXCollections.observableArrayList();
+        try {
+            Leaderboard leaderboard = openRankingFile();
+             for (RankingVal value : leaderboard.get(this.levelNumber)) {
+                values.add(value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(values.get(0).getDateString());
         return values;
     }
 }
@@ -108,10 +153,10 @@ class Leaderboard {
 
     public void add(RankingVal value, int level) {
         switch (level) {
-            case 1: addToLevel1(value);
-            case 2: addToLevel1(value);
-            case 3: addToLevel1(value);
-            case 4: addToLevel1(value);
+            case 1: addToLevel1(value); break;
+            case 2: addToLevel2(value); break;
+            case 3: addToLevel3(value); break;
+            case 4: addToLevel4(value); break;
         }
     }
 
@@ -127,6 +172,25 @@ class Leaderboard {
     private void addToLevel4(RankingVal value) {
         this.level1.add(value);
     }
+    public List<RankingVal> get(int n) {
+        //List<RankingVal> default = new ArrayList<RankingVal>();
+        switch (n) {
+            case 1: return getLevel1();
+            case 2: return getLevel2();
+            case 3: return getLevel3();
+            case 4: return getLevel4();
+        }
+        return null;
+    }
+    public void set(List<RankingVal> leveln, int n) {
+        switch (n) {
+            case 1: setLevel1(leveln); break;
+            case 2: setLevel2(leveln); break;
+            case 3: setLevel3(leveln); break;
+            case 4: setLevel4(leveln); break;
+        }
+    }
+
     public void setLevel1(List<RankingVal> level1) {
         this.level1 = level1;
     }
@@ -183,10 +247,8 @@ class RankingVal {
     public long getDate() {
         return this.date;
     }
-    public String getTime() {
-        long minutes = this.time / 60;
-        long seconds = this.time % 60;
-        return "" + minutes + ":" + seconds;
+    public long getTime() {
+        return this.time;
     }
     public double getCorrect() {
         return this.correct;
@@ -206,9 +268,17 @@ class RankingVal {
     public String getDateString() {
         long millis = this.date * 1000;
         Date date = new Date(millis);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss", Locale.ENGLISH);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
         sdf.setTimeZone(TimeZone.getTimeZone("UTC+1"));
         return sdf.format(date);
+    }
+    public String getTimeString() {
+        long minutes = this.time / 60;
+        long seconds = this.time % 60;
+        return "" + minutes + ":" + seconds;
+    } 
+    public String getCorrectString() {
+        return "" + (int)(this.correct*100) + "%";
     }
 
 }
