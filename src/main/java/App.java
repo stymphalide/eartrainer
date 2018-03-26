d4725eecc2e99b01c249903a3a2a6c0b9f931a0f
stymphalide <angelo.birrer@gmail.com>
2018-03-27 01:25:35 +0200

    Finished the App class.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,66 @@
+import java.util.*;
+
+import javafx.application.Application;      // Application class from which JavaFX application extend. [JavaFX API]
+import javafx.stage.Stage;                  // The JavaFX Stage class is the top level JavaFX container. The primary Stage is constructed by the platform. Additional Stage objects may be constructed by the application. [JavaFX API]
+import javafx.event.ActionEvent;            // An Event representing some type of action. [JavaFX]
+import javafx.event.EventHandler;           // Handler for events of a specific class / type. [JavaFX]
+import javafx.scene.control.Button;         // A simple Button Control. Can be a event Target and Contains text and/or graphic [JavaFX API].
+import javafx.scene.Scene;                  // The JavaFX Scene class is the container for all content in a scene graph. The background of the scene is filled as specified by the fill property. [JavaFX API]
+import javafx.scene.input.MouseEvent;       // When mouse event occurs, the top-most node under cursor is picked and the event is delivered to it through capturing and bubbling phases described at EventDispatcher. [JavaFX]
+import javafx.scene.image.Image;            // The Image class represents graphical images and is used for loading images from a specified URL. [JavaFX]
+import javafx.scene.image.ImageView;        // The ImageView is a Node used for painting images loaded with Image class. [JavaFX]
+import javafx.scene.control.TextField;      // Text input component that allows a user to enter a single line of unformatted text. [JavaFX]
 /*
-    Date: 20.01.2018
+    Date: 28.03.2018
     Project Name: Eartrainer
     Names: Angelo Birrer G4L and Tobias Seefeld G4L
-    Main Sources: TBD
+    Main Sources: JavaFX API
     Code Management: logic: Tobias Seefeld G4L | view: Angelo Birrer G4L
 */
 /* classdoc
-    This class holds the whole application together. It inherits from the javafx Application.
-    The class has the following attributes:
-    - Menu menu;
-    - Stage window;
-    - Button backToMenu;
-    - Button startLevel1;
-    - Button confirm;
 
-    There are two public methods:
-    - void main(String[] args);
-    - void start(Stage primaryStage);
+    -- INSTANCE VARIABLES --
+    This class holds the whole application together. It inherits from the javafx Application class.
+    The class has the following attributes:
+     1. Stage window
+    
+     2. Button backToMenu
+     3. List<Button> startLevels
+     4. Button confirm
+     5. Button viewRanking
+     6. TextField userInput
+     7. Button help
+     8. ImageView musicToggler
+
+     9. view.Menu menu
+    10. logic.Level level
+    11. view.Music musicThread
+
+    The first attribute is the the actual window, where most parts of the game are rendered in.
+    The next seven instance variables are control elements, that are used in several methods or that are passed to other objects as parameters.
+    Most of them have some kind of action set that is called when clicked.
+
+    The last three are objects of other classes we have created. 
+    The menu is the holder for what one sees in the menu screen.
+    The level variable holds the level object when one clicks on a startLevel button.
+    The musicThread variable is a thread that is independent from the main javaFX thread, this allows for playing music without the menu being frozen.
+
+
+    -- METHODS ---
+    -- public --
+    There are four public methods:
+    - void main(String[] args)
+    - void init()
+    - void start(Stage primaryStage)
+    - void stop()
 
     The content of the main method is just `Application.launch(args)`. This statement starts the javafx application lifecycle.
     The lifecycle consists of five steps:
         1. Instanciation of the Application class.
-        2. The init phase: call the init() method. 
+        2. The init phase: call the init() method.
+            This calls the public `init()` method.
+            This method can be overriden in the class.
+            It then can be used to set any instance variables of the application, since there cannot be a custom constructor.
         3. The start phase:
             This calls the public `start(Stage primaryStage)` method.
             This method needs to be overriden in the class.
@@ -30,48 +69,75 @@
         4. The exit phase:
             Makes sure the process stops after all windows have been closed or after `Platform.exit()` is being called somewhere.
         5. Calls the stop() method:
-            'This method is called when the application should stop, 
+            Here all other open threads are destroyed.
+            Since '[t]his method is called when the application should stop, 
             and provides a convenient place to prepare for application exit and destroy resources.'
             [JavaFX API]
 
+    The `void init()` method handles the proper initialisation of the application. 
+    Most things that are usually defined in an object's constructor are defined here.
+    The init method:
+    - populates the startLevels list and adds event listentes to those buttons.
+    - adds events to all other buttons including a click event for the sound image.
+    - instantiates the menu object.
+
+    One thing to note here is that most events are actually just calling a private method of the class. 
+    This is to keep the code there a little bit tighter.
+
+
     The `void start(Stage primaryStage)` method handles the view of the game.
     - First the argument of the method (primaryStage) is set to the class attribute window, 
-        this allows it to be referenced from several parts of the class.
-    - Next it sets an EventHandler for the startLevel1 Button. This makes sure that every time this button is pressed,
-        the setUpLevel1 is pressed.
-    - Next it sets an EventHandler for the backToMenu Button. This makes sure the setUpMenu() method is called upon pressing.
-    - Then a menu is actually rendered (setUpMenu).
-    - Last it tells the window to show itself: Attempts to show this Window by setting visibility to true [JavaFX API].
+        this allows it to be referenced from all parts of the class.
+    - Then it starts the music by calling the private `startMusic()` method
+    - Next it calls the `setUpMenu` method, which is responsible for properly rendering the menu screen.
+    - Lastly it calls window.show() which makes the Stage object show its contents.
 
-    There are two private methods:
-    - void setUpMenu();
-    - void setUpLevel1();
-    Those methods serve as helpers for the `start()` method.
-    
-    The `setUpMenu()` method renders a Menu view (as defined in view.Menu).
-    And adds it to the window. Which means it is being shown in the X window of the application.
 
-    The `setUpLevel1()` method renders a Level view (as defined in view.Level).
-    It furthermore creates a new instance of the logic.Level(1) class.
-    It also adds EventListeners to the confirm button.
-    This allows the view to be re-rendered after a question has been answered and
-    gives the possibility to check whether the level is over and then render the afterGame View.
+    -- private --
+    There are six private methods:
+    1. void startMusic()
+    2. void stopMusic
+    3. void setUpMenu()
+    4. void setUpLevel(int n)
+    5. void setUpRanking()
+    6. void setUpHelp()
+
+    Those methods serve as helpers for the `start()`, `init()` and the `stop()` methods respectively.
+
+    The `startMusic()` method creates a new thread, where the music is played. 
+    However, this thread is only created if the player did not turn the music off.
+
+    The `stopMusic()` cancels the music if it is still playing.
+
+    The `setUpMenu()` makes the already instantiated menu create a Scene object and adds the scene to the window.
+    Which means it is being shown in the X window of the application. Additionally it changes the title of the window.
+
+    The `setUpLevel(int n)`:
+    - stops the music thread.
+    - instantiates a logic.Level object.
+    - changes the title of the window.
+    - creates a view.Level object, which is basically a VBox.
+    - sets an eventListener for the confirm button. Which if clicked:
+        - Gets the answer the user gave. And checks whether all values are given.
+        - It then sets the players answer as the answer
+        - calls the `level.nextQuestion()` method, which validates the users answer and sets a new question.
+        - If the level is over, it:
+            - opens a popup window that asks for the name
+            - After hitting submit it closes the window and opens the leaderboard, where the game should be present.
+            - In the after game screen the music should play again.
+            - the title of the window changes again.
+            - the level view is transformed to the after game screen.
+        - Otherwise it just updates the level view.
+    - makes the view.level object render a scene and sets it as the main scene of the window.
+
+    The `setUpRanking()` method opens a new window called "Leaderboard" that shows a ranking scene as described in the view.Ranking class.
+
+    The `setUpHelp()` method opens a new window called "Help!" that shows a help text as described in the view.Help class.
 */
 /*
     Main Source: JavaFX API
 */
 
-import javafx.application.Application; // Application class from which JavaFX application extend. [JavaFX API]
-import javafx.scene.control.Button;    // A simple Button Control. Can be a event Target and Contains text and/or graphic [JavaFX API].
-import javafx.scene.Scene;             // The JavaFX Scene class is the container for all content in a scene graph. The background of the scene is filled as specified by the fill property. [JavaFX API]
-import javafx.stage.Stage;             // The JavaFX Stage class is the top level JavaFX container. The primary Stage is constructed by the platform. Additional Stage objects may be constructed by the application. [JavaFX API]
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import java.util.*;
-import javafx.scene.control.TextField;
 
 public class App extends Application {
     Stage window;
@@ -145,10 +211,10 @@ public class App extends Application {
         window = primaryStage;
         startMusic();
         setUpMenu();
-        //primaryStage.setMaximized(true); // Maximise the window.
         window.show();
     }
 
+    @Override
     public void stop() throws Exception {
         stopMusic();
     }
@@ -159,7 +225,6 @@ public class App extends Application {
             this.musicThread.start();
         }
     }
-
     private void stopMusic() {
         if(this.musicThread != null) {
             this.musicThread.cancel();
@@ -167,40 +232,41 @@ public class App extends Application {
     }
 
     private void setUpMenu() {
-        window.setTitle("eartrainer - Menu");
-        window.setScene(this.menu.render());
+        this.window.setTitle("eartrainer - Menu");
+        this.window.setScene(this.menu.render());
     }
+    
     private void setUpLevel(int n) {
         stopMusic();
         this.level = new logic.Level(n);
-
-        window.setTitle("eartrainer - Level " + level.getLevelNumber());
-
+        this.window.setTitle("eartrainer - Level " + level.getLevelNumber());
+        
         view.Level levelView = new view.Level(level, this.confirm);
 
         this.confirm.setOnAction(e -> {
             List<String> values = levelView.getComboBoxValues();
-            logic.Card answer = new logic.Card(values.get(0), values.get(1), values.get(2), values.get(3));
-            level.setAnswer(answer);
-            level.nextQuestion();
-            if (level.isFinished()) {
-                this.userInput = new TextField();
-                Button submit = new Button();
-                final Stage popUp = new Stage();
-                submit.setOnAction(event -> {
-                    String username = userInput.getText();
-                    view.Ranking.updateRanking(level, username);
-                    popUp.close();
-                    setUpRanking();
-                });
-                popUp.setScene(view.Ranking.getPopUp(submit, this.userInput));
-                popUp.show();
-                window.setTitle("eartrainer - Game Over");
-                startMusic();
-                levelView.viewFinished(level, backToMenu, startLevels.get(n - 1), this.musicToggler);                
+            if(values == null) {
+                System.out.println("Set All Combobox Values!");
             } else {
-                if (values == null) { // Only advance if all comboboxes are set.
-                    System.out.println("Set All Combobox Values!");
+                logic.Card answer = new logic.Card(values.get(0), values.get(1), values.get(2), values.get(3));
+                level.setAnswer(answer);
+                level.nextQuestion();
+                if (level.isFinished()) {
+                    this.userInput = new TextField();
+                    Button submit = new Button();
+                    final Stage popUp = new Stage();
+                    submit.setOnAction(event -> {
+                        String username = userInput.getText();
+                        view.Ranking.updateRanking(level, username);
+                        popUp.close();
+                        setUpRanking();
+                    });
+                    popUp.setScene(view.Ranking.getPopUp(submit, this.userInput));
+                    popUp.setTitle("Name");
+                    popUp.show();
+                    window.setTitle("eartrainer - Game Over");
+                    startMusic();
+                    levelView.viewFinished(level, backToMenu, startLevels.get(n - 1), this.musicToggler);                
                 } else {
                     levelView.update();
                 }
@@ -208,19 +274,23 @@ public class App extends Application {
         });
         window.setScene(levelView.render());
     }
+    
     private void setUpRanking() {
         final Stage ranking = new Stage();
         view.Ranking rankingScene = new view.Ranking();
         ranking.setScene(rankingScene.render());
+        ranking.setTitle("Leaderboard");
         ranking.show();
     }
+    
     private void setUpHelp() {
         final Stage helpView = new Stage();
         view.Help helpScene = new view.Help();
         helpView.setScene(helpScene.render());
+        helpView.setTitle("Help!");
         helpView.show();
     }
-    
+
     public static void main(String[] args) {
         Application.launch(args);
     }
