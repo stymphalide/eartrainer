bec9ce8277593d35034e95ea2738963999719d8d
Tobias <tobias@bimbam.ch>
2018-03-28 00:16:25 +0200

    Commented the Sound class.

--- src/test/java/logic/SoundTest.java
+++ src/test/java/logic/SoundTest.java
@@ -7,6 +7,6 @@ public class SoundTest {
     @Test public void testCardToStaccato() {
     	Card card = new Card("Piano", "Upwards", "Middle", "Perfect Fifth");
         Sound classUnderTest = new Sound();
-        assertEquals("the following Staccato-String is expected", "V0 I[Piano] [48]h [55]h", classUnderTest.cardToStaccato(card));
+        assertEquals("the following Staccato-String is expected", "V0 I[Piano] [48]h [55]h", classUnderTest.cardToMusicString(card));
     }
 }
